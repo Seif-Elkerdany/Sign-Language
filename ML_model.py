05bcd0a16d57cd378a413a0f405c81b6a3236440
Seif Elkerdany <145226996+Seif-Elkerdany@users.noreply.github.com>
2024-02-21 15:34:49 +0200

    Update ML_model.py

--- ML_model.py
+++ ML_model.py
@@ -6,16 +6,16 @@ from sklearn.neural_network import MLPClassifier
 import pickle
 
 #reading the data
-df = pd.read_csv(r"C:\Users\Saif Elkerdany\MyTraining_Data.csv",header = None)
+df = pd.read_csv(r"MyTraining_Data.csv", header = None)
 
 #pre-procssing
-X = df.drop(columns= [42])
+X = df.drop(columns = [42])
 Y = df[42]
 
-x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size = 0.2,shuffle=True)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, shuffle = True)
 
 #model
-clf = MLPClassifier(random_state=2, max_iter=1000,activation = "tanh" , alpha = 0.5).fit(x_train, y_train)
+clf = MLPClassifier(random_state = 2, max_iter=1000, activation = "tanh" , alpha = 0.5).fit(x_train, y_train)
 y_predict = clf.predict(x_test)
 
 #acurracy
